b694c187ec8a482877474b0ce4cf87256c25252b
kristjanmoora <148195692+kristjanmoora@users.noreply.github.com>
2024-04-10 10:12:53 +0300

    Update main.py

--- Mängu jookustamine/main.py
+++ Mängu jookustamine/main.py
@@ -2,27 +2,29 @@ import pygame
 import sys
 pygame.init()
 
-#värvid
-lGreen = [153, 255, 153]
-lBlue = [153, 204, 255]
+# Määrame mõned värvid RGB kujul
+lGreen = [153, 255, 153]  
+lBlue = [153, 204, 255]  
 
-#ekraani seaded
-screen=pygame.display.set_mode([640,480])
+
+screen = pygame.display.set_mode([640, 480])
 pygame.display.set_caption("Harjutamine")
+
+# Täidame ekraani heleda rohelise värviga
 screen.fill(lGreen)
 
+# Mäng jätkub seni, kuni gameover muutuja väärtus on False
 gameover = False
 
 while not gameover:
+    # Lisame pildid
+    youWin = pygame.image.load("youwin.png")  # Laeme pildi failist youwin.png
+    youWin = pygame.transform.scale(youWin, [540, 360])  # Muudame pildi suurust
+    screen.blit(youWin, [50, 100])  # Kuvame pildi ekraanile teatud asukohas
 
-    #Lisame pildid
-    youWin = pygame.image.load("youwin.png")
-    youWin = pygame.transform.scale(youWin, [540, 360])
-    screen.blit(youWin,[50,100])
+    pygame.display.flip()  # Värskendame akent
 
-    pygame.display.flip()
-
-    #mängu sulgemine ristist
-    for i in pygame.event.get():
-       if i.type == pygame.QUIT:
-           sys.exit()
+    # Mängu sulgemine ristist
+    for event in pygame.event.get():  # Käime läbi kõik sündmused
+        if event.type == pygame.QUIT:  # Kui sündmuse tüüp on "QUIT"
+            sys.exit()  # Siis väljume programmist
